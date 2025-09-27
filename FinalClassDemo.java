f067fc52cc75c185905559e8de0d3e0cd10fbe8c
Prince-kumar223 <prince.kumar.08.ju@gmail.com>
2025-09-27 14:22:58 +0530

    commit first

---
+++ FinalClassDemo.java
@@ -0,0 +1,19 @@
+final class Vehicle {
+    public void display() {
+        System.out.println("This is a final class Vehicle.");
+    }
+}
+public class FinalClassDemo {
+    public static void main(String[] args) {
+        Vehicle v = new Vehicle();
+        v.display();
+    }
+}
+
+
+
+
+
+
+
+
